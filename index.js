b1e9117c74ea317a39b35c3416f4dfa89ed0e0f7
Anton Belko <30528165+ko1p@users.noreply.github.com>
2021-04-14 11:07:01 +0300

    remake LinkedList in Typescript

--- index.js
+++ index.js
@@ -1,114 +1,88 @@
 // [data, next] -> [data, next] -> [data, next] 
-
-class Node {
-    constructor(data, next = null) {
-        this.data = data
-        this.next = next
+var NodeEl = /** @class */ (function () {
+    function NodeEl(data, next) {
+        this.data = data;
+        this.next = next;
     }
-}
-
-class LinkedList {
-    constructor() {
-        this.head = null
-        this.tail = null
+    return NodeEl;
+}());
+var LinkedList = /** @class */ (function () {
+    function LinkedList() {
+        this.head = null;
+        this.tail = null;
     }
-
-    append(data) {
-        const node = new Node(data)
-
+    LinkedList.prototype.append = function (data) {
+        var node = new NodeEl(data);
         if (this.tail) {
-            this.tail.next = node
+            this.tail.next = node;
         }
-
         if (!this.head) {
-            this.head = node
+            this.head = node;
         }
-
-        this.tail = node
-    }
-
-    prepend(data) {
-        const node = new Node(data, this.head)
-
-        this.head = node
-
+        this.tail = node;
+    };
+    LinkedList.prototype.prepend = function (data) {
+        var node = new NodeEl(data, this.head);
+        this.head = node;
         if (!this.tail) {
-            this.tail = node
+            this.tail = node;
         }
-    }
-
-    toArray() {
-        let current = this.head
-        const output = []
-
+    };
+    LinkedList.prototype.toArray = function () {
+        var current = this.head;
+        var output = [];
         while (current) {
-            output.push(current)
-            current = current.next
+            output.push(current);
+            current = current.next;
         }
-
-        return output
-    }
-
-    find(data) {
+        return output;
+    };
+    LinkedList.prototype.find = function (data) {
         if (!this.head) {
-            return
+            return;
         }
-
-        let current = this.head
-
+        var current = this.head;
         while (current) {
             if (current.data = data) {
-                return current
+                return current;
             }
-            current = current.next
+            current = current.next;
         }
-
-    }
-
-    insertAfter(after, data) {
-        const found = this.find(after)
-
+    };
+    LinkedList.prototype.insertAfter = function (after, data) {
+        var found = this.find(after);
         if (!found) {
-            return
+            return;
         }
-
-        const node = new Node(data, found.next)
-
-        found.next = node
-    }
-
-
-    remove(data) {
+        var node = new NodeEl(data, found.next);
+        found.next = node;
+    };
+    LinkedList.prototype.remove = function (data) {
         if (!this.head) {
-            return
+            return;
         }
-
         while (this.head && this.head.data === data) {
-            this.head = this.head.next
+            this.head = this.head.next;
         }
-
-        let current = this.head
-
+        var current = this.head;
         while (current.next) {
-            if(current.next.data === data) {
-                current.next = current.next.next
-            } else {
-                current = current.next
+            if (current.next.data === data) {
+                current.next = current.next.next;
+            }
+            else {
+                current = current.next;
             }
         }
-
         if (this.tail.data === data) {
-            this.tail = current
+            this.tail = current;
         }
-
-    }
-}
-
-const list = new LinkedList()
-list.append('One')
-list.append('two')
-list.prepend('Zero')
-list.prepend('0')
+    };
+    return LinkedList;
+}());
+var list = new LinkedList();
+list.append('One');
+list.append('two');
+list.prepend('Zero');
+list.prepend('0');
 // list.remove('0')
-
-console.log(list.toArray())
+console.log(list.toArray());
