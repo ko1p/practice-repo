ff249bf19b9053972f0b532c889ddf04ff9227fe
Anton Belko <30528165+ko1p@users.noreply.github.com>
2021-04-23 15:08:48 +0300

    add logger

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-<<<<<<< HEAD
-import React, {useState} from 'react';
-import { Counter }  from './components/Counter'
-import { Increaser }  from './components/Increaser'
-import { Logger }  from './components/Logger'
-=======
 import React from 'react';
->>>>>>> 32994f0ac8e3a4f984d954f26881afc5660a5260
+import { Logger } from './components/Logger'
 
-export const App: React.FC = (): React.ReactElement => {
+const App: React.FC = (): React.ReactElement => {
 
   return (
     <>
-<<<<<<< HEAD
       <h1>Какой-то очень важный сайт. </h1>
-      <Counter counter={counter} />
-      <Increaser setCounter={setCounter} />
       <Logger />
-=======
-      <h1>Наше приложение</h1>
->>>>>>> 32994f0ac8e3a4f984d954f26881afc5660a5260
     </>
   );
 }
+
+export default App;
