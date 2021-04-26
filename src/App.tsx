20a8397e0bcef2049fa71522748059bd3ec83b46
Anton Belko <30528165+ko1p@users.noreply.github.com>
2021-04-26 11:48:47 +0300

    remove old components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import { Logger } from './components/Logger'
 
 const App: React.FC = (): React.ReactElement => {
 
   return (
     <>
       <h1>Какой-то очень важный сайт. </h1>
-      <Logger />
+
     </>
   );
 }
